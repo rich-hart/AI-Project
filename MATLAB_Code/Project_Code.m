0011394afa16ce498b15daa45e6820bf79d9bf25
Rich Hart <richhohart@gmail.com>
2013-01-22 17:29:17 -0500

    Trying to make a movie

--- MATLAB_Code/Project_Code.m
+++ MATLAB_Code/Project_Code.m
@@ -1,11 +1,14 @@
 %Get Video Data
 VideoData = VideoReader('SampleVideos/car12.mov');
 
+MovieFrames(VideoData.NumberOfFrames) = struct('cdata',[],'colormap',[]);
+
+for n = 1:VideoData.NumberOfFrames
 %Read the first frame from the video
-FirstFrame = read(VideoData,1);
+VideoFrame = read(VideoData,n);
 
 %Convert Image to GrayScale
-GrayScaleImage = rgb2gray(FirstFrame);
+GrayScaleImage = rgb2gray(VideoFrame);
 
 %find edges
 BW_Edges=edge(GrayScaleImage);
@@ -13,17 +16,20 @@ BW_Edges=edge(GrayScaleImage);
 %find corners
 corner_positions=corner(GrayScaleImage);
 
+[m,n] = size(corner_positions);
 
+for i = 1:m
+    y=corner_positions(i,1);
+    x=corner_positions(i,2);
+    
+    VideoFrame(x,y,1)=255;
+     VideoFrame(x,y,2)=0;
+      VideoFrame(x,y,3)=0;
+end
+f =im2frame( VideoFrame);
+MovieFrames(n)=f;
+end
 
+movie(MovieFrames);
 
-
-
-
-
- 
-
-   figure; imshow(GrayScaleImage); hold on 
-
-
-  % Overlay valid interest points
-    plot(corner_positions(:,1), corner_positions(:,2), 'y.')
+%figure; imshow(VideoFrame);
