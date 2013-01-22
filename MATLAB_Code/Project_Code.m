151dbe8ba6e04ff42cf0f4197494c7d703e2ab80
Rich Hart <richhohart@gmail.com>
2013-01-22 15:59:22 -0500

    Corner Detection

--- MATLAB_Code/Project_Code.m
+++ MATLAB_Code/Project_Code.m
@@ -2,9 +2,25 @@
 VideoData = VideoReader('SampleVideos/car12.mov');
 
 %Read the first frame from the video
-FirstFrame = read(VideoData,1)
+FirstFrame = read(VideoData,1);
 
-%Read an Image
-TestCellImage = imread('SampleImages/spatia8.gif');
-%Find Edges in that Image
-BW = edge(TestCellImage);
+%Convert Image to GrayScale
+GrayScaleImage = rgb2gray(FirstFrame);
+
+%find edges
+BW_Edges=edge(GrayScaleImage);
+
+%find corners
+corner_positions=corner(GrayScaleImage);
+
+[m,n] = size(corner_positions);
+
+for i = 1:m
+   x=corner_positions(m,1);
+   y=corner_positions(m,2);
+   GrayScaleImage(x,y)=0;
+end
+
+
+markerInserter = vision.MarkerInserter 
+J = step(markerInserter,GrayScaleImage,corner_positions);
